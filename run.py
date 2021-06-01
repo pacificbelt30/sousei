e95676c73e7171ba10d8a1685616f0e3672883c5
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-06-02 01:48:18 +0900

    flaskというかpythonのパッケージがわからなすぎて多分死んでるし確実に死んでる

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy, SessionBase
-
-app = Flask(__name__)
-app.config.from_pyfile('instance/config/settings.py')
-db = SQLAlchemy(app)
-
+import app
+app.run()
 
