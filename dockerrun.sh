03be95360e97b3e80d9a23dac31fd86c6d3d407e
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-07-23 04:01:11 +0900

    なんとかdocker-compose up -d だけでデータベースのテーブルの作成以外をできるようになった

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`:/var/flaskapp -p 2000:5000 --network sousei_default -it --rm sousei_nginx /bin/bash
+docker run -v `pwd`:/var/flaskapp -p 2000:80 --network sousei_default -it --rm sousei_nginx /bin/bash
