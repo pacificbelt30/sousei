4cbe7180fcea6a3587ebdd57a5c988434f7d6771
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-07-23 22:57:27 +0900

    データベースの作成からやるとmariadbがスタンバイする前にアクセスし，データベースが作成されなかったため，そこを修正

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,12 @@
 #cd /var/flask_app && uwsgi_python3 --ini uwsgi.ini & nginx -g "daemon off;"
-cd /var/flask_app && uwsgi_python3 --ini uwsgi.ini & nginx -g "daemon off;"
+cd /var/flaskapp
+python3 run.py -mb
+uwsgi_python3 --ini uwsgi.ini & 
+nginx -g "daemon off;" 
 #nginx -g "daemon off;"
+#python3 run.py -mb
 /bin/bash
 #cd /var/flask_app
 #python3 run.py
 # GRANT SELECT,INSERT,UPDATE ON hoge.* TO sousei@'localhost';
+
