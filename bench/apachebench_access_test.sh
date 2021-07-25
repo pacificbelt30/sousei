3aeeeb33f30e0cdc53181b1a8ae5322d706239b1
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-07-26 02:08:06 +0900

    touch-reload追加

--- bench/apachebench_access_test.sh
+++ bench/apachebench_access_test.sh
@@ -4,7 +4,9 @@
 #ab -n 1000 -c 50 http://localhost:5000/kamoku/F1?kyoin=P011
 #ab -n 1000 -c 10 http://localhost:5002/kamoku/F1?kyoin=P011
 #ab -n 1000 -c 50 http://localhost:2000/kamoku/F1?kyoin=P011
-ab -n 1000 -c 60 http://localhost/bench/F1
+#ab -n 1000 -c 60 http://localhost/bench/F1
+ab -n 1000 -c 60 http://192.168.1.8:13431/bench/F1
+#ab -n 1000 -c 60 http://localhost/
 #ab -n 1000 -c 50 http://localhost/bench/F2
 
 #loginpage
